216db996d52959631fb2019f52165e89293bc254
Hassaan <muhammadhassaannaveed904@gmail.com>
2024-03-16 06:40:23 +0500

    chnages in memberProfile

--- js/MemberProfile.js
+++ js/MemberProfile.js
@@ -22,6 +22,16 @@ function showMember(memberId) {
   firebase.auth().onAuthStateChanged(async function (user) {
     if (user) {
       try {
+
+    let mem_reg = document.getElementById('member_regno');
+    let mem_name = document.getElementById('member_name');
+    let mem_age = document.getElementById('member_age');
+    let mem_email = document.getElementById('member_email');
+    let mem_gender = document.getElementById('member_gender');
+    let mem_contact = document.getElementById('member_num');
+    let mem_bmi = document.getElementById('member_bmi');
+    let mem_image = document.getElementById('image_container');
+
         const memberRef = db.collection('members')
           .where('adminId', '==', user.uid)
           .where('id', '==', memberId);
@@ -32,14 +42,13 @@ function showMember(memberId) {
           const doc = querySnapshot.docs[0];
           const memberData = doc.data();
           console.log('Member Data:', memberData);
-
-          memberDetailsDiv.innerHTML = `
-            <p><strong>Name:</strong> ${memberData.name}</p>
-            <p><strong>Age:</strong> ${memberData.age}</p>
-            <p><strong>Phone:</strong> ${memberData.phoneNumber}</p>
-            <p><strong>Email:</strong> ${memberData.email}</p>
-            <img src="${memberData.pictureUrl}" alt="Member Picture">
-          `;
+          mem_reg.innerText = memberData.rfid;
+          mem_name.innerText = memberData.name;
+          mem_age.innerText = memberData.age;
+          mem_email.innerText = memberData.email;
+          mem_contact.innerText = memberData.phoneNumber;
+          mem_image.innerHTML = `<img class='card-img' src='${memberData.pictureUrl}' /> ` 
+          createChart()
         } else {
           memberDetailsDiv.innerHTML = "<p>Member not found.</p>";
         }
@@ -50,6 +59,34 @@ function showMember(memberId) {
     }
   });
 }
+
+const createChart = () => {
+  const ctx = document.getElementById('myChart');
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+  const chartData = {
+      labels: labels,
+      datasets: [{
+          label: 'BMI Trend',
+          data: [44,55,66,33,55,33,44],
+          fill: false,
+          borderColor: 'rgb(220, 53, 69)',
+          pointBackgroundColor: 'rgb(220, 53, 69)',
+          pointBorderColor: 'rgb(220, 53, 69)',
+          pointColor: 'rgb(220, 53, 69)',
+          pointStrokeColor: 'rgb(220, 53, 69)',
+          tension: 0.01,
+       }],
+  };
+
+  const chartConfig = {
+      type: 'line',
+      data: chartData,
+  };
+
+  new Chart(ctx, chartConfig);
+};
+
 
 const urlParams = new URLSearchParams(window.location.search);
 const memberId = urlParams.get('memberId');
